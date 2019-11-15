4d02d1a7a834f30ef24d088231932c44c3070656
ptv6ug <ptv6ug@virginia.edu>
2019-11-14 19:33:35 -0500

    tab stuff

---
+++ skej_v1/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+class Icon extends React.Component {
+  render() {
+    const { name, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+      </View>
+    );
+  }
+}
+
+const HomeIcon = props => {
+  // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+  return <Icon {...props} />;
+};
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+    IconComponent = HomeIcon;
+  } else if (routeName === 'Settings') {
+    iconName = `ios-options${focused ? '' : '-outline'}`;
+  }
+
+  // You can return any component that you like here!
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+export default createAppContainer(
+  createBottomTabNavigator(
+    {
+      Home: { screen: HomeScreen },
+      Settings: { screen: SettingsScreen },
+    },
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) =>
+          getTabBarIcon(navigation, focused, tintColor),
+      }),
+      tabBarOptions: {
+        activeTintColor: '#6FC4FA',
+        inactiveTintColor: 'gray',
+      },
+    }
+  )
+);
+
---
+++ skej_v1/components/Splash.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants, Image } from 'expo';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+export default class Splash extends React.Component {
+  render() {
+    return (
+        <LinearGradient 
+       start={[1,1]}
+       end={[0.4, 0.3]}
+        colors={[ '#FFFFFF', '#000D0C', '#000D0C',]} 
+        style={styles.gradientStyles}>
+          <Text style={styles.logo}> Skej </Text>
+      </LinearGradient>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  gradientStyles:{
+   flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo:{
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+    height:100,
+  }
+})
