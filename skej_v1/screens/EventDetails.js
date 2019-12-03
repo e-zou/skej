7962096e32036bc6b8fe6d6495cb297e7a9904ea
ptv6ug <ptv6ug@virginia.edu>
2019-12-03 18:05:35 -0500

    image on eventDetails

--- skej_v1/screens/EventDetails.js
+++ skej_v1/screens/EventDetails.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
 export default class EventDetails extends Component {
   render() {
     const { navigation } = this.props;
     return (
-      <View>
-        <Text style={styles.eventName}>
-          {navigation.getParam('name', 'NO-NAME')}
-        </Text>
-        <Text>
-          {navigation.getParam('date', 'NO-DATE')}
-        </Text>
+      <View style={styles.container}>
+        <Image source={{ uri: navigation.getParam('pic', 'NO-IMAGE') }} style={styles.imageContainer}/>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.eventName}>
+            {navigation.getParam('name', 'NO-NAME')}
+          </Text>
+          <Text>
+            {navigation.getParam('date', 'NO-DATE')}
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
   eventName: {
     fontSize: 18,
     fontWeight: 'bold'
+  },
+  detailsContainer: {
+    padding: 10,
+  },
+  imageContainer: {
+    width: Dimensions.get('window').width*0.8,
+    height: Dimensions.get('window').height*0.25,
+    padding: 20,
   }
 });
